04d935e8a12ce918690802fef9b474bf5e6ef239
Alec Burgdorf <aeburgd@gmail.com>
2018-09-17 13:54:19 -0500

    move main functions to utils.js

---
+++ src/utils.js
@@ -0,0 +1,32 @@
+// Utils for bot.js
+import {RichEmbed} from 'discord.js'
+import fetch from 'isomorphic-fetch'
+import {dogUrl} from '../config.json'
+
+const splitUrls = urlString => urlString.split(/,+/g)
+const buildEmbed = url => new RichEmbed({image: {url}})
+
+/* eslint-disable require-await */
+export async function dog(msg, num) {
+  fetch(`${dogUrl}/${num}`)
+    .then(data => data.json())
+    .then(json => Array.of(json.message))
+    .then(message => {
+      message.forEach(url => {
+        url = url.toString()
+        const urls = splitUrls(url)
+        const embeds = new Array(urls.length)
+        urls.forEach(url => {
+          embeds.push(buildEmbed(url))
+        })
+        embeds.forEach(embed => {
+          msg.channel.send(embed)
+        })
+      })
+    })
+    .catch(error => {
+      msg.channel.send('Sorry, couldn\'t quite get that.')
+      throw error
+    })
+}
+/* eslint-enable require-await */
